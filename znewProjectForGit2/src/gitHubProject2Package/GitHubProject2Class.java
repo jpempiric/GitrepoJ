b1a8ee7562cf12365fd2f2f84b3e7a5b76b5e207
jagdish <abc.gmail.com>
2024-06-26 16:10:14 +0530

    modified

--- znewProjectForGit2/src/gitHubProject2Package/GitHubProject2Class.java
+++ znewProjectForGit2/src/gitHubProject2Package/GitHubProject2Class.java
@@ -3,6 +3,8 @@ package gitHubProject2Package;
 public class GitHubProject2Class {
 	public static void main(String[] args) {
 		System.out.println("Hello Git for the Project 2");
+		System.out.println("hi git 2 nd times");
+		System.out.println("3rd time");
 	}
 
 }
