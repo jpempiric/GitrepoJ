c366a94d0d67684fa0884277af1927fcb5ed216f
jagdish <abc.gmail.com>
2024-06-26 16:15:00 +0530

    tenth time Gui

--- znewProjectForGit2/src/gitHubProject2Package/GitHubProject2Class.java
+++ znewProjectForGit2/src/gitHubProject2Package/GitHubProject2Class.java
@@ -6,6 +6,12 @@ public class GitHubProject2Class {
 		System.out.println("hi git 2 nd times");
 		System.out.println("3rd time");
 		System.out.println("4th times");
+		System.err.println("6th time for git gui");
+		
+		
+		
+		
+		System.out.println("10th time for gui ");
 	}
 
 }
--- znewProjectForGit2/src/gitHubProject2Package/GitHubProject2Class2.java
+++ znewProjectForGit2/src/gitHubProject2Package/GitHubProject2Class2.java
@@ -5,6 +5,10 @@ public class GitHubProject2Class2 {
 	public static void main(String[] args) {
 		System.out.println("Hello Git for the Project 2");
 		System.out.println("2nd line for class 2");
+		System.out.println("6th times for  git gui");
+		
+		
+		System.out.println("10th time for gui ");
 	}
 
 }
