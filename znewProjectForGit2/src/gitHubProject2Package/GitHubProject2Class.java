04943e75f6dda95ae29e4409c377c896d29eefda
jagdish <abc.gmail.com>
2024-06-26 16:06:57 +0530

    adding project 2 for git

---
+++ znewProjectForGit2/src/gitHubProject2Package/GitHubProject2Class.java
@@ -0,0 +1,8 @@
+package gitHubProject2Package;
+
+public class GitHubProject2Class {
+	public static void main(String[] args) {
+		System.out.println("Hello Git for the Project 2");
+	}
+
+}
---
+++ znewProjectForGit2/src/gitHubProject2Package/GitHubProject2Class2.java
@@ -0,0 +1,11 @@
+package gitHubProject2Package;
+
+public class GitHubProject2Class2 {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Git for the Project 2");
+	}
+
+}
+
+
